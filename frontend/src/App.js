3876dcc637ae2ec8a0643c9987b4d58638310c0d
ArnM1077 <arnm1077@gmail.com>
2024-02-12 21:57:03 -0500

    appbar and toolbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import {ThemeProvider} from "@mui/material";
 import theme from "./components/theme";
+import Header from "./components/header";
 
 
 const App = () => {
   return (
       <ThemeProvider theme={theme}>
-
+        <Header/>
       </ThemeProvider>
   )
 }
---
+++ frontend/src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import { Box, Toolbar, Typography } from "@mui/material";
+
+export default function Header(){
+    <AppBar>
+        <Toolbar>
+            <Typography>hello</Typography>
+        </Toolbar>
+    </AppBar>
+}
